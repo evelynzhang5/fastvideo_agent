4c096f4bf82aac84fbabc81bbcfba479bf7795f5
evelynzhang5 <evelynzhang0516@gmail.com>
2026-03-24 17:12:12 -0700

    add facts

--- core/agent_loop.py
+++ core/agent_loop.py
@@ -2,13 +2,16 @@ from core.skill_matcher import match_skill
 from core.memory_loader import load_memory
 
 def run(task):
-    memory = load_memory(task)
-    skill = match_skill(task)
 
-    response = f"""
-Task: {task}
-Memory: {memory}
-Selected Skill: {skill}
+    memory = load_memory(task)
+
+    answer = f"""
+Based on FastVideo documentation:
+
+{memory}
+
+Answer:
+{memory.splitlines()[-1]}
 """
 
-    return response
+    return answer
